b620f81220de070e21626271ab72a373f6096d1a
nerooc <tomasz_gajda@outlook.com>
2025-01-30 21:47:55 +0100

    test global library refactoring

--- src/__sandbox__/index.tsx
+++
@@ -1,6 +0,0 @@
-import { App } from './App';
-import React from 'react';
-import { render } from 'react-dom';
-
-const appMount = document.querySelector('#app');
-if (appMount) render(<App />, appMount);
---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+    plugins: [react()],
+    build: {
+        lib: {
+            entry: 'src/index.tsx',
+            name: 'Rerousel',
+            fileName: (format) => `rerousel.${format}.js`,
+            formats: ['es', 'cjs'],
+        },
+        rollupOptions: {
+            external: ['react', 'react-dom'],
+            output: {
+                globals: {
+                    react: 'React',
+                    'react-dom': 'ReactDOM',
+                },
+            },
+        },
+        target: 'esnext',
+        sourcemap: true,
+    },
+});
